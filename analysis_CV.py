cb58d77e485025fe3f9489a067c6e4c65c02ff90
linneabjork <linnea.bjork@live.se>
2024-01-07 11:32:01 +0100

    Adding new functions + formatting

--- analysis_CV.py
+++ analysis_CV.py
@@ -1,6 +1,6 @@
 # Python script to automatize analysis of stability and activity measurements
 
-#import of modules
+# import of modules
 import numpy as np
 import math
 import matplotlib.pyplot as plt
@@ -12,47 +12,54 @@ import csv
 
 
 # Extract voltage and current from data file
-def get_voltage_current(filepath):                  
-    with open(filepath, 'r') as file:
+def get_voltage_current(filepath):
+    with open(filepath, "r") as file:
         data = file.readlines()
         i = 0
-        for line in data:                             #loop through the lines in the file
-            newline = line.rstrip("\n").split("\t")   #split the columns into a list & remove trailing characters (ex blank space) after new line
+        for line in data:  # loop through the lines in the file
+            newline = line.rstrip("\n").split(
+                "\t"
+            )  # split the columns into a list & remove trailing characters (ex blank space) after new line
             data[i] = newline
             i += 1
-        
-        del data[0:9]                            #information about the measurement, remove from data
+
+        del data[0:9]  # information about the measurement, remove from data
 
         Vraw = []
         Iraw = []
-        for i in range(len(data)): 
-            Vraw.insert(i, float(data[i][1].replace(',','.')))      #string to float. Use "."as decimal sign
-            Iraw.insert(i, float(data[i][2].replace(',','.')))    
+        for i in range(len(data)):
+            Vraw.insert(
+                i, float(data[i][1].replace(",", "."))
+            )  # string to float. Use "."as decimal sign
+            Iraw.insert(i, float(data[i][2].replace(",", ".")))
     return Vraw, Iraw
+
 
 # Store the background measurement in a matrix where each column corresponds to one variable
 def get_background(path_background):
-    with open(path_background, 'r') as file:
+    with open(path_background, "r") as file:
         background_data = file.readlines()
-        
+
         i = 0
-        for line in background_data: 
-            newline = line.rstrip("\n").rstrip("\t\t").split("\t") 
+        for line in background_data:
+            newline = line.rstrip("\n").rstrip("\t\t").split("\t")
             background_data[i] = newline
             i += 1
 
-        background_matrix = np.zeros((len(background_data),len(background_data[0])))
+        background_matrix = np.zeros((len(background_data), len(background_data[0])))
 
-        for i in range(len(background_data)):        #loop through rows first
-            for j in range (len(background_data[i])):
-                background_matrix[i][j] = float(background_data[i][j].replace(',','.'))
+        for i in range(len(background_data)):  # loop through rows first
+            for j in range(len(background_data[i])):
+                background_matrix[i][j] = float(background_data[i][j].replace(",", "."))
     return background_matrix
+
 
 # Store the measurement parameters in a dictionary
 def get_parameters(path_parameters):
-    with open(path_parameters, 'r') as file:
+    with open(path_parameters, "r") as file:
         parameters_dictionary = json.loads(file.read())
     return parameters_dictionary
+
 
 # Correction to RHE potential with voltage as a list
 def RHE_correction(voltage, background_matrix, parameters_dictionary):
@@ -60,5 +67,38 @@ def RHE_correction(voltage, background_matrix, parameters_dictionary):
         e = e - parameters_dictionary["reference"]
 
     for i in range(len(background_matrix[0])):
-        background_matrix[0][i] = background_matrix[0][i] - parameters_dictionary["reference"]
+        background_matrix[0][i] = (
+            background_matrix[0][i] - parameters_dictionary["reference"]
+        )
     return voltage, background_matrix
+
+
+# IR-drop correction. index is used to choose the correct column in background_parameters,
+# 2023-01-04: index is either 3 or 5
+def ir_drop_correction(voltage, background_matrix, index, parameters_dictionary):
+    for i in range(len(voltage)):
+        voltage[i] = (
+            voltage[i] - background_matrix[i][index] * parameters_dictionary["ir_comp"]
+        )
+    return voltage
+
+
+# Subtracting background from current
+def background_correction_current(current, background_matrix, index):
+    for i, e in enumerate(current):
+        e = e - background_matrix[i][index]
+    return current
+
+
+# Normalizing values to geometric surface area and mass
+def normalizing(voltage, current, parameters_dictionary):
+    mass = (
+        parameters_dictionary["loading"] * parameters_dictionary["A_geo"]
+    )  # mg/cm2 * cm2 = mg
+
+    mass_V = np.array(voltage) * 1000 / mass
+    mass_I = np.array(current) * 1000 / mass
+    surface_V = np.array(voltage) * 1000 / parameters_dictionary["A_geo"]
+    surface_I = np.array(current) * 1000 / parameters_dictionary["A_geo"]
+
+    return mass_V, mass_I, surface_V, surface_I
